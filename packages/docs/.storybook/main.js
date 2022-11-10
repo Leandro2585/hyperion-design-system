2def13dbae571ec5f435b31780ce445b992839b6
leandro2585 <leo.real2585@gmail.com>
2022-11-10 08:57:24 -0300

    fix: add subpath to storybook on production environment

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -15,5 +15,11 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if(configType === 'PRODUCTION') {
+      config.base = '/hyperion-design-system'
+    }
+    return config
   }
 }
